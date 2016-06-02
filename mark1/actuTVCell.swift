92619152019e2060116f8e40728210688d402e21
mjosse <mjosse@student.42.fr>
2016-06-02 16:43:42 +0200

    refonte homePage

---
+++ mark1/actuTVCell.swift
@@ -0,0 +1,30 @@
+//
+//  actuTVCell.swift
+//  mark1
+//
+//  Created by Josse on 02/06/2016.
+//  Copyright © 2016 mjosse. All rights reserved.
+//
+
+import UIKit
+
+class actuTVCell: UITableViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var rating: ratingControl!
+    @IBOutlet weak var outfit: UILabel!
+    @IBOutlet weak var likeCount: UILabel!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
